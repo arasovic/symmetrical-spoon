a660fef87c7ed880c522344e105faa0cee78a437
arasovic <arasmehmet07@gmail.com>
2021-12-03 21:39:15 +0300

    here we start again

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 2,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+  trailingComma: 'none'
+};
