30b88055f50d4614f9faffaa7085d6190d7d6f7a
arasovic <arasmehmet07@gmail.com>
2021-12-08 19:22:37 +0300

    added footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,70 +4,168 @@ import {
   Divider,
   Grid,
   Header,
-  List,
+  Image,
   Segment
 } from 'semantic-ui-react'
+import imgIos from '@assets/ios/ios Bitmap@2x.png'
+import imgAnd from '@assets/and/and Bitmap@2x.png'
+import imgInst from '@assets/instagram/instagram@2x.png'
+import imgFace from '@assets/facebook/facebook@2x.png'
+import imgTwit from '@assets/twitter/twitter@2x.png'
+import imgBitmap from '@assets/bitmap/Bitmap@2x.png'
+import imgArrow from '@assets/arrow down/Arrow_simple_down@2x.png'
 
 const Footer: React.FC = () => {
   return (
     <Segment
       vertical
-      style={{ margin: '5em 0em 0em', padding: '5em 0em', background: 'white' }}
+      style={{ margin: '5em 0em 0em', padding: '3em 0em', background: 'white' }}
     >
-      <Container textAlign='center'>
-        <Grid divided stackable>
-          <Grid.Column width={3}>
-            <Header as='h4' content='Group 1' />
-            <List link>
-              <List.Item as='a'>Link One</List.Item>
-              <List.Item as='a'>Link Two</List.Item>
-              <List.Item as='a'>Link Three</List.Item>
-              <List.Item as='a'>Link Four</List.Item>
-            </List>
+      <Container>
+        <Grid columns='equal' stackable>
+          <Grid.Column>
+            <Segment basic>
+              <Grid centered>
+                <Image src={imgIos} size='small' />
+                <Image src={imgAnd} size='small' />
+              </Grid>
+            </Segment>
           </Grid.Column>
-          <Grid.Column width={3}>
-            <Header as='h4' content='Group 2' />
-            <List link>
-              <List.Item as='a'>Link One</List.Item>
-              <List.Item as='a'>Link Two</List.Item>
-              <List.Item as='a'>Link Three</List.Item>
-              <List.Item as='a'>Link Four</List.Item>
-            </List>
-          </Grid.Column>
-          <Grid.Column width={3}>
-            <Header as='h4' content='Group 3' />
-            <List link>
-              <List.Item as='a'>Link One</List.Item>
-              <List.Item as='a'>Link Two</List.Item>
-              <List.Item as='a'>Link Three</List.Item>
-              <List.Item as='a'>Link Four</List.Item>
-            </List>
-          </Grid.Column>
-          <Grid.Column width={7}>
-            <Header as='h4' content='Footer Header' />
-            <p>
-              Extra space for a call to action inside the footer that could help
-              re-engage users.
-            </p>
+          <Grid.Column width={5} />
+          <Grid.Column>
+            <Segment basic>
+              <Grid textAlign='right' centered>
+                <Image
+                  src={imgInst}
+                  size='tiny'
+                  className='footer-social-buttons'
+                />
+                <Image
+                  src={imgFace}
+                  size='tiny'
+                  className='footer-social-buttons'
+                />
+                <Image
+                  src={imgTwit}
+                  size='tiny'
+                  className='footer-social-buttons'
+                />
+              </Grid>
+            </Segment>
           </Grid.Column>
         </Grid>
-
         <Divider section />
-        {/* <Image centered size='mini' src='/logo.png' /> */}
-        <List horizontal divided link size='small'>
-          <List.Item as='a' href='#'>
-            Site Map
-          </List.Item>
-          <List.Item as='a' href='#'>
-            Contact Us
-          </List.Item>
-          <List.Item as='a' href='#'>
-            Terms and Conditions
-          </List.Item>
-          <List.Item as='a' href='#'>
-            Privacy Policy
-          </List.Item>
-        </List>
+        <Grid columns='equal' divided stackable>
+          <Grid.Column width={3}>
+            <Segment basic>
+              <Image src={imgBitmap} size='tiny' className='footer-image' />
+              <Divider clearing />
+              <a href='mailto:Lipsum@lorem.com'>Lipsum@lorem.com</a>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column width={13}>
+            <Segment basic>
+              <Grid stackable>
+                <Grid.Row columns={3} centered>
+                  <Grid.Column>
+                    <Header as='h4' className='footer-header'>
+                      Lorem İpsum
+                    </Header>
+                    <hr />
+                    <div className='footer-links'>
+                      <a href='#a'>Lipsum</a>
+                      <a href='#a'>Doloressini</a>
+                      <a href='#a'>Amedissu</a>
+                    </div>
+                  </Grid.Column>
+                  <Grid.Column>
+                    <Header as='h4' className='footer-header'>
+                      Doloressini
+                    </Header>
+                    <hr />
+                    <div className='footer-links'>
+                      <a href='#a'>
+                        Lorem İpsum doloressini
+                        <Image
+                          src={imgArrow}
+                          inline
+                          size='mini'
+                          className='footer-arrow'
+                        />
+                      </a>
+                      <a href='#a'>
+                        Lorem İpsum dolores
+                        <Image
+                          src={imgArrow}
+                          inline
+                          size='mini'
+                          className='footer-arrow'
+                        />
+                      </a>
+                      <a href='#a'>Lorem İpsum</a>
+                      <a href='#a'>Lorem İpsum dolores</a>
+                    </div>
+                  </Grid.Column>
+                  <Grid.Column>
+                    <Header as='h4' className='footer-header'>
+                      Ameddesso
+                    </Header>
+                    <hr />
+                    <div className='footer-links'>
+                      <a href='#a' className='bold'>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                      </a>
+                      <a href='#a'>
+                        Doloressini
+                        <Image
+                          src={imgArrow}
+                          inline
+                          size='mini'
+                          className='footer-arrow'
+                        />
+                      </a>
+                    </div>
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </Segment>
+          </Grid.Column>
+        </Grid>
+        <Divider section />
+        <Container textAlign='center' fluid text>
+          <Grid centered>
+            <Grid.Row centered columns={3} divided>
+              <Grid.Column>
+                <Image
+                  src={imgBitmap}
+                  centered
+                  size='tiny'
+                  className='footer-image'
+                />
+              </Grid.Column>
+              <Grid.Column>
+                <Image
+                  src={imgBitmap}
+                  centered
+                  size='tiny'
+                  className='footer-image'
+                />
+              </Grid.Column>
+              <Grid.Column>
+                <Image
+                  src={imgBitmap}
+                  centered
+                  size='tiny'
+                  className='footer-image'
+                />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
+        <Divider section />
+        <Container text textAlign='center'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </Container>
       </Container>
     </Segment>
   )
--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -31,12 +31,16 @@ const GlobalStyles = createGlobalStyle`
       flex-direction: column !important;
     }
   }
-  
-  .shadow{
-    -moz-box-shadow: 0 0 11px -2px #666; !important;
-    -webkit-box-shadow: 0 0 11px -2px #666; !important;
-    box-shadow: 0 0 11px -2px #666; !important;
-    border-radius: 19px 19px; !important;
+
+  .shadow {
+    -moz-box-shadow: 0 0 11px -2px #666;
+  !important;
+    -webkit-box-shadow: 0 0 11px -2px #666;
+  !important;
+    box-shadow: 0 0 11px -2px #666;
+  !important;
+    border-radius: 19px 19px;
+  !important;
     padding: 10px !important;
   }
 
@@ -59,6 +63,10 @@ const GlobalStyles = createGlobalStyle`
 
   .ui.label.product-card-details-image > img {
     height: 1.5em !important;
+  }
+
+  .ui.label.product-card-details-image > span {
+    color: #918b8b !important;
   }
 
   .product-card-pre-discount-text {
@@ -86,6 +94,52 @@ const GlobalStyles = createGlobalStyle`
 
   .product-card-add-basket-button:hover {
     filter: grayscale(15%) !important;
+  }
+
+  .footer-social-buttons {
+    width: 5.5em !important;
+  }
+
+  .footer-links > a {
+    padding: 2px 0 3px 0;
+  }
+
+  .bold {
+    font-weight: bold !important;
+  }
+
+  hr {
+    margin: -10px 0 10px 0;
+    width: 100%;
+    height: 2px;
+    background-color: rgba(34, 36, 38, .15);
+    border: 0;
+  }
+
+  hr:after {
+    content: '';
+    display: block;
+    border-bottom: 2px solid #db2828;
+    max-width: 35%;
+  }
+
+  a {
+    display: block;
+    color: black;
+    text-decoration: none;
+  }
+
+  a:hover {
+    color: black;
+  }
+
+  .footer-arrow {
+    width: 15px !important;
+    margin-left: 3px !important;
+  }
+
+  .footer-image {
+    width: 60px !important;
   }
 
 `
